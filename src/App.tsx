877050463f335aba9ee0f888a3f299dee4bfc9df
abdullah0sadiku <Avdullahsadiku02@gmail.com>
2025-07-16 10:04:14 +0200

    Fix GitHub Pages routing with HashRouter and 404.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login';
 import Homepage1 from './Homepage1';
 import Homepage2 from './Homepage2';
